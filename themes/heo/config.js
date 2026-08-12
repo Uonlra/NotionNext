48cea96ec1eb9271be3279d6f51135f52df71f15
Uon1ra <120707623+Uonlra@users.noreply.github.com>
2026-08-12 22:52:08 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -6,7 +6,7 @@ const CONFIG = {
 
   HEO_INFO_CARD_AVATAR_BLUR: true, // 文章详情页个人资料卡头像样式。true：显示为模糊装饰头像；false：与首页头像保持一致
 
-  HEO_COLOR_PRIMARY: '#7c8cff'',       // 主按钮
+  HEO_COLOR_PRIMARY: '#7c8cff',       // 主按钮
   HEO_COLOR_PRIMARY_HOVER: '#8178eb', // 主色悬停：稍亮紫色
   HEO_COLOR_PRIMARY_TEXT: '#ffffff',
   
